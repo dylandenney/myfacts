4322317eb5ad92db037b68237796961096a39fbe
Dylan Denney <dylan.denney@globalpay.com>
2017-03-27 16:35:36 -0400

    adding gl for SandySprings

--- lib/facter/datacenter.rb
+++ lib/facter/datacenter.rb
@@ -4,7 +4,7 @@ Facter.add(:datacenter) do
     location = Facter.value(:hostname)[0..1]
     case location
     when 'g1' then datacenter = 'AtlantaMetro'
-    when 'g3' then datacenter = 'SandySprings'
+    when 'g3','gl' then datacenter = 'SandySprings'
     when 'v1' then datacenter = 'Richmond'
 #  (I was testing this in azure)  when 'pu' then datacenter = 'azurepup'
     else 'unknown'
