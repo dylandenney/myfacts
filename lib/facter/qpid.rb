f36cf95a798cfa2ac7c81d5ffd8f3d861faa2daf
Dylan Denney <dylan.denney@globalpay.com>
2017-07-21 13:47:49 -0400

    fixin the grep

--- lib/facter/qpid.rb
+++ lib/facter/qpid.rb
@@ -1,6 +1,6 @@
 require 'facter'
 
-result = %x{/bin/rpm -qa |grep qpid | grep server.*client.*qmf}
+result = %x{/bin/rpm -qa |grep qpid | grep -vE 'python|ssl|tools'}
 
 Facter.add('qpid') do
     setcode do
