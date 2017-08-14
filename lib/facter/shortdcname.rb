28b9b215423bc308c338c4b10484efe4ae1c5681
Dylan Denney <dylan.denney@globalpay.com>
2017-08-14 11:36:28 -0400

    updating some data for generic use

--- lib/facter/shortdcname.rb
+++ lib/facter/shortdcname.rb
@@ -3,8 +3,8 @@ Facter.add(:shortdcname) do
   setcode do
     location = Facter.value(:hostname)[1]
     case location
-    when '1' then shortdcname = 'atl'
-    when '3','gl' then shortdcname = 'ss'
+    when '1' then shortdcname = 'd1'
+    when '3','gl' then shortdcname = 'd3'
     else 'unknown'
     end
   end
